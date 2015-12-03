b8d3310affd30efa42fe5c6b1fc09da14d31ce60
nakamuraruk <nakamuraruk@nttdata.co.jp>
2015-12-03 16:19:53 +0900

    defined funcs to read xls sheet

--- src/scribble/core.clj
+++ src/scribble/core.clj
@@ -1,6 +1,23 @@
-(ns scribble.core)
+(ns scribble.core
+  (:require [clojure.java.io :as io])
+  (:import 
+    (org.apache.poi.ss.usermodel WorkbookFactory)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- load-workbook [efile]
+  (WorkbookFactory/create (io/file efile)))
+
+(defn- get-sheet [sheetname] 
+  (.getSheet sheetname))
+
+(defn- get-row [sheet rownum]
+  (.getRow sheet rownum))
+
+(comment 
+  (def what-the-fuck [root]
+    (let [rt (io/file root)
+          children (.listFiles rt)]
+      (for [c children]
+        (cond
+          (.isFile c) (println (.getName c)))
+        ))))
+  

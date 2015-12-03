9b11189d3c88c871b078e7de643f23e0ba082a3d
nakamuraruk <nakamuraruk@nttdata.co.jp>
2015-12-03 13:56:47 +0900

    change license

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject scribble "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :license {:name "Do What The Fuck You Want To Public License"
+            :url "http://www.wtfpl.net/"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.apache.poi/poi "3.13"]])

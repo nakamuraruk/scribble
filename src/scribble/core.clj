de978cf4de9b04c1944b8f42bc7ba0b453fa7175
nakamuraruk <nakamuraruk@nttdata.co.jp>
2015-12-11 14:52:15 +0900

    make functions public

--- src/scribble/core.clj
+++ src/scribble/core.clj
@@ -3,21 +3,11 @@
   (:import 
     (org.apache.poi.ss.usermodel WorkbookFactory)))
 
-(defn- load-workbook [efile]
+(defn load-workbook [efile]
   (WorkbookFactory/create (io/file efile)))
 
-(defn- get-sheet [sheetname] 
+(defn get-sheet [sheetname] 
   (.getSheet sheetname))
 
-(defn- get-row [sheet rownum]
+(defn get-row [sheet rownum]
   (.getRow sheet rownum))
-
-(comment 
-  (def what-the-fuck [root]
-    (let [rt (io/file root)
-          children (.listFiles rt)]
-      (for [c children]
-        (cond
-          (.isFile c) (println (.getName c)))
-        ))))
-  
